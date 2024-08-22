9815a535b676ff025d34f5ff83d6e85c10efbcbd
Tanner Steorts <tanner.steorts@icloud.com>
2024-08-22 17:32:26 -0600

    Eyes Nose and Mouth

--- GuardGoose/Character.cs
+++ GuardGoose/Character.cs
@@ -4,66 +4,66 @@ using System.Threading;
 
 public partial class Character : CharacterBody3D
 {
-    [Export] public float moveSpeed = 5.0f;
-    [Export] public float jumpForce = 10.0f;
-    [Export] public float gravity = -9.8f;
-    [Export] public float inputBufferTime = 0.2f;
+	[Export] public float moveSpeed = 5.0f;
+	[Export] public float jumpForce = 10.0f;
+	[Export] public float gravity = -9.8f;
+	[Export] public float inputBufferTime = 0.2f;
 	private float jumpBufferTimeRemaining = 0.0f;
 	private Godot.Camera3D camera;
 	private Node3D cameraController;
 	private Node3D cameraTarget;
 
-    public override void _Ready()
-    {
-        cameraController = GetNode<Node3D>("CharacterCameraNode");
-        cameraTarget = cameraController.GetNode<Node3D>("CharacterCameraTarget");
-        camera = cameraTarget.GetNode<Godot.Camera3D>("CharacterCamera");
-    }
+	public override void _Ready()
+	{
+		cameraController = GetNode<Node3D>("CharacterCameraNode");
+		cameraTarget = cameraController.GetNode<Node3D>("CharacterCameraTarget");
+		camera = cameraTarget.GetNode<Godot.Camera3D>("CharacterCamera");
+	}
 
 	private void HandleInputBuffer(float delta)
-    {
-        if (jumpBufferTimeRemaining > 0)
-        {
-            jumpBufferTimeRemaining -= delta;
-        }
-        if (Input.IsActionJustPressed("ui_accept"))
-        {
-            jumpBufferTimeRemaining = inputBufferTime;
-        }
-    }
+	{
+		if (jumpBufferTimeRemaining > 0)
+		{
+			jumpBufferTimeRemaining -= delta;
+		}
+		if (Input.IsActionJustPressed("ui_accept"))
+		{
+			jumpBufferTimeRemaining = inputBufferTime;
+		}
+	}
 
 	private void HandleMovement(float delta)
-    {
+	{
 		Vector3 velocity = Velocity;
 
-        Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-        Vector3 moveDirection = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+		Vector3 moveDirection = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
 
-        velocity.X = moveDirection.X * moveSpeed;
-        velocity.Z = moveDirection.Z * moveSpeed;
+		velocity.X = moveDirection.X * moveSpeed;
+		velocity.Z = moveDirection.Z * moveSpeed;
 
-        // Apply gravity
-        if (!IsOnFloor())
-        {
-            velocity.Y += gravity * delta * 3;
-        }
-        else
-        {
-            velocity.Y = 0; 
+		// Apply gravity
+		if (!IsOnFloor())
+		{
+			velocity.Y += gravity * delta * 3;
+		}
+		else
+		{
+			velocity.Y = 0; 
 
-            if (jumpBufferTimeRemaining > 0)
-            {
-                velocity.Y = jumpForce;
-                jumpBufferTimeRemaining = 0;
-            }
-        }
+			if (jumpBufferTimeRemaining > 0)
+			{
+				velocity.Y = jumpForce;
+				jumpBufferTimeRemaining = 0;
+			}
+		}
 
 		Velocity = velocity;
-        MoveAndSlide();
-    }
+		MoveAndSlide();
+	}
 
-    public override void _PhysicsProcess(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		HandleInputBuffer((float)delta);
 		HandleMovement((float)delta);
