075fc0c8d833a8008e924d3f41b459fb5af736b2
Colton Weeding <cweeding1@gmail.com>
2024-08-23 17:29:56 -0600

    resolving conflicts

--- GuardGoose/.godot/mono/temp/obj/Debug/GuardGooseGame.AssemblyInfo.cs
+++ GuardGoose/.godot/mono/temp/obj/Debug/GuardGooseGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuardGooseGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea6522494f03784afa1511b5541281d1b110c39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuardGooseGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuardGooseGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
