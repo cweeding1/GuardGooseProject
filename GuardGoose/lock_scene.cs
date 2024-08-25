5f60a6e594353f11cbcd893d9cfdee7444131364
Colton Weeding <cweeding1@gmail.com>
2024-08-25 17:02:26 -0600

    current changes

--- GuardGoose/lock_scene.cs
+++ GuardGoose/lock_scene.cs
@@ -3,36 +3,41 @@ using System;
 
 public partial class lock_scene : Node2D
 {
-    private Node2D lockRoot;
-    private Sprite2D keySprite;
+	private Node2D lockRoot;
+	private Sprite2D keySprite;
+	private Label keyLabel;
+   
     private float angle;
     private float length = 200;
     private float width = 25;
-    private Vector2[] points;
+	private Vector2[] rectanglePoints;
 
-    public override void _Ready()
-    {
-        lockRoot = GetNode<Node2D>("LockRoot");
-        keySprite = lockRoot.GetNode<Sprite2D>("KeySprite");
-        keySprite.LookAt(GetGlobalMousePosition());
+
+	private int keys = 3;
+
 
         int rnd = new Random().Next(0,360);
         angle = Mathf.DegToRad(rnd);
 
-        points = _GeneratePickArea(width, length);
-        
-        keySprite.LookAt(GetGlobalMousePosition());
-        
-        lockRoot = GetNode<Node2D>("LockRoot");
-        GD.Print(lockRoot.Position);
-    }
+		//Setup
+
+		int rnd = new Random().Next(0,360);
+		angle = Mathf.DegToRad(rnd);
+
+		rectanglePoints = _GeneratePickArea(width, length);
+		
+		keySprite.LookAt(GetGlobalMousePosition());
+
+		keyLabel = GetNode<Label>("KeyLabel");
+		_SetKeyText();
+	}
 
     public override void _Draw()
     {
-        DrawLine(points[0], points[1], new Color(1, 0, 0), 2);  // Red line
-        DrawLine(points[1], points[2], new Color(1, 0, 0), 2);  // Red line
-        DrawLine(points[2], points[3], new Color(1, 0, 0), 2);  // Red line
-        DrawLine(points[3], points[0], new Color(1, 0, 0), 2);  // Red line
+        DrawLine(rectanglePoints[0], rectanglePoints[1], new Color(1, 0, 0), 2);  // Red line
+        DrawLine(rectanglePoints[1], rectanglePoints[2], new Color(1, 0, 0), 2);  // Red line
+        DrawLine(rectanglePoints[2], rectanglePoints[3], new Color(1, 0, 0), 2);  // Red line
+        DrawLine(rectanglePoints[3], rectanglePoints[0], new Color(1, 0, 0), 2);  // Red line
     }
 
     public override void _Process(double delta)
@@ -48,7 +53,7 @@ public partial class lock_scene : Node2D
         if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
         {
             Vector2 mousePosition = GetGlobalMousePosition();
-            if (Geometry2D.IsPointInPolygon(mousePosition, points))
+			if (Geometry2D.IsPointInPolygon(mousePosition, rectanglePoints))
             {
                 GD.Print("IN");
                 _GenerateNewSmallerPickArea();
@@ -56,14 +61,24 @@ public partial class lock_scene : Node2D
             else
             {
                 GD.Print("OUT");
+				if(keys > 0)
+				{
+					keys -= 1;
+				}
+				_SetKeyText();
             }
         }
     }
 
-    private Vector2[] _GeneratePickArea(float width, float length)
-    {
-        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).Normalized();
-        Vector2 perpendicular = new Vector2(-direction.Y, direction.X).Normalized() * (width / 2);
+	private void _SetKeyText()
+	{
+		keyLabel.Text = $"Keys: {keys}";
+	}
+
+	private Vector2[] _GeneratePickArea( float width, float length )
+	{
+		Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).Normalized();
+		Vector2 perpendicular = new Vector2(-direction.Y, direction.X).Normalized() * (width / 2);
 
         Vector2 corner1 = perpendicular;
         Vector2 corner2 = -perpendicular;
